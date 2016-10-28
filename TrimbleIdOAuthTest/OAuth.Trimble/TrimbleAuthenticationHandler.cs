cdd1f1c58a52f5df2cb50d3e7c0c0a8eed3619a3
Andreas Lang <Andreas_Lang@trimble.com>
2016-10-28 16:25:55 +0200

    Valid base64 decoding

--- TrimbleIdOAuthTest/OAuth.Trimble/TrimbleAuthenticationHandler.cs
+++ TrimbleIdOAuthTest/OAuth.Trimble/TrimbleAuthenticationHandler.cs
@@ -22,23 +22,22 @@ namespace AspNet.Security.OAuth.Trimble {
         }
 
 
-        /*
         protected override async Task<AuthenticationTicket> CreateTicketAsync(ClaimsIdentity identity,
             AuthenticationProperties properties, OAuthTokenResponse tokens) {
             // Note: access tokens and request keys are passed in the querystring for Trimble
-            var address = QueryHelpers.AddQueryString(Options.UserInformationEndpoint, new Dictionary<string, string>() {
-                ["access_token"] = tokens.AccessToken,
-                ["key"] = Options.RequestKey,
-                ["site"] = Options.Site
-            });
+            //var address = QueryHelpers.AddQueryString(Options.UserInformationEndpoint, new Dictionary<string, string>() {
+            //    ["access_token"] = tokens.AccessToken,
+            //    ["key"] = Options.RequestKey,
+            //    ["site"] = Options.Site
+            //});
 
-            var request = new HttpRequestMessage(HttpMethod.Get, address);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //var request = new HttpRequestMessage(HttpMethod.Get, address);
+            //request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
-            response.EnsureSuccessStatusCode();
+            //var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
+            //response.EnsureSuccessStatusCode();
 
-            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+            //var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
 
             // Note: the email claim cannot be retrieved from Trimble's user information endpoint.
             // Todo: implement
@@ -46,16 +45,17 @@ namespace AspNet.Security.OAuth.Trimble {
             //        .AddOptionalClaim(ClaimTypes.Name, TrimbleAuthenticationHelper.GetDisplayName(payload), Options.ClaimsIssuer)
             //        .AddOptionalClaim(ClaimTypes.Webpage, TrimbleAuthenticationHelper.GetWebsiteUrl(payload), Options.ClaimsIssuer)
             //        .AddOptionalClaim("urn:Trimble:link", TrimbleAuthenticationHelper.GetLink(payload), Options.ClaimsIssuer);
+            //identity.AddClaims(ClaimTypes.NameIdentifier, TrimbleAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer);
 
-            var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
+            //var principal = new ClaimsPrincipal(identity);
+            //var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
 
-            var context = new OAuthCreatingTicketContext(ticket, Context, Options, Backchannel, tokens, payload);
-            await Options.Events.CreatingTicket(context);
+            //var context = new OAuthCreatingTicketContext(ticket, Context, Options, Backchannel, tokens, payload);
+            //await Options.Events.CreatingTicket(context);
 
-            return context.Ticket;
+            //return context.Ticket;
+            return null;
         }
-        */
 
         protected override async Task<OAuthTokenResponse> ExchangeCodeAsync(string code, string redirectUri) {
 
@@ -92,75 +92,110 @@ namespace AspNet.Security.OAuth.Trimble {
             Dictionary<string, string> responseVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(responsestr);
 
             //JsonConvert
-            var content = QueryHelpers.ParseQuery(responsestr);
-            var payload = new JObject();
-            foreach (var item in content) {
+            JObject payload = new JObject();
+            //var content = QueryHelpers.ParseQuery(responsestr);
+            //foreach (var item in content)
+            foreach (var item in responseVals) {
                 string ikey = (string)item.Key;
                 string ival = (string)item.Value;
                 //payload[item.Key] = (string) item.Value;
+                payload[ikey] = ival;
             }
 
             //JToken id_token = payload.GetValue("id_token");
-            string sIdtoken;
-            responseVals.TryGetValue("id_token", out sIdtoken);
+            string sIdtokenEnc;
+            responseVals.TryGetValue("id_token", out sIdtokenEnc);
 
-            sIdtoken = "eyJhbGciOiJSUzI1NiJ9.eyJodHRwOlwvXC93c28yLm9yZ1wvY2xhaW1zXC9pZGVudGl0eVwvdW5sb2NrVGltZSI6IjAiLCJzdWIiOiJhbmRyZWFzX2xhbmdAdHJpbWJsZS5jb20iLCJodHRwOlwvXC93c28yLm9yZ1wvY2xhaW1zXC9hY2NvdW50bmFtZSI6InRyaW1ibGUuY29tIiwiYXpwIjoiSEE3NG02UFBZN1NzX19zejBVTVVER2ltTVlZYSIsImh0dHA6XC9cL3dzbzIub3JnXC9jbGFpbXNcL2ZpcnN0bmFtZSI6IkFuZHJlYXMiLCJhdF9oYXNoIjoibDBJQ21MNjRORndRY2FNNVVYb2R3dyIsImlzcyI6Imh0dHBzOlwvXC9pZGVudGl0eS1zdGcudHJpbWJsZS5jb20iLCJodHRwOlwvXC93c28yLm9yZ1wvY2xhaW1zXC9sYXN0bmFtZSI6IkxhbmciLCJodHRwOlwvXC93c28yLm9yZ1wvY2xhaW1zXC90ZWxlcGhvbmUiOiIrNDk4OTg5MDU3MTQ4NCIsImh0dHA6XC9cL3dzbzIub3JnXC9jbGFpbXNcL3V1aWQiOiI2YmM5YTllMS1hNTI3LTQ0NWMtYWNlNS1lZTEyMTZkNmRjMjgiLCJpYXQiOjE0Nzc1Nzg3NzcsImh0dHA6XC9cL3dzbzIub3JnXC9jbGFpbXNcL2dpdmVubmFtZSI6IkFuZHJlYXMiLCJhdXRoX3RpbWUiOjE0Nzc1NzQ1MDAsImV4cCI6MTQ3NzU4MjM3NywiaHR0cDpcL1wvd3NvMi5vcmdcL2NsYWltc1wvaWRlbnRpdHlcL2ZhaWxlZExvZ2luQXR0ZW1wdHMiOiIwIiwiaHR0cDpcL1wvd3NvMi5vcmdcL2NsYWltc1wvaWRlbnRpdHlcL2FjY291bnRMb2NrZWQiOiJmYWxzZSIsImh0dHA6XC9cL3dzbzIub3JnXC9jbGFpbXNcL2NvdW50cnkiOiJHZXJtYW55IiwiYXVkIjpbIkhBNzRtNlBQWTdTc19fc3owVU1VREdpbU1ZWWEiXSwiaHR0cDpcL1wvd3NvMi5vcmdcL2NsYWltc1wvZW1haWxhZGRyZXNzIjoiYW5kcmVhc19sYW5nQHRyaW1ibGUuY29tIiwiaHR0cDpcL1wvd3NvMi5vcmdcL2NsYWltc1wvYWNjb3VudHVzZXJuYW1lIjoiYW5kcmVhc19sYW5nIn0.pI82";
-            char[] sIdtokenChars = sIdtoken.ToCharArray();
-            //XBase64.Base64Decoder(sIdtokenChars);
-            XBase64.Base64Decoder myDecoder = new XBase64.Base64Decoder(sIdtokenChars);
-            byte[] temp = myDecoder.GetDecoded();
-            //Base64Decoder myDecoder = new Base64Decoder(data);
-
-            StringBuilder sb = new StringBuilder();
-
-            //byte[] temp = myDecoder.GetDecoded();
-            sb.Append(System.Text.UTF8Encoding.UTF8.GetChars(temp));
-            //sb.Append(System.Text.ASCIIEncoding.ASCII.GetChars(temp));
-
-            string sTmpDecoded = sb.ToString();
-
-
-            //char[] carr = sIdtoken.ToCharArray();
-            //int len = carr.Length;
-            //byte[] convbytes = Convert.FromBase64CharArray(carr, 0, len);
-
-            //byte[] inbytes = Encoding.ASCII.GetChars()
-            //int len = inbytes.Length;
-            //byte[] convbytes = Convert.FromBase64CharArray(inbytes, 0, len);
-
-            //string idtokenstr = (string) payload.GetValue("id_token");
-            //string sIdJson = Convert.FromBase64String(sIdtoken).ToString();
-            byte[] convbytes;
-            try
+            // To use directly Convert.FromBase64String leads to invalid character exception.
+            // This is because there are illegal characters in it (".").
+            // That's why we use this as separator for making substrings. One of it includes the information we want.
+            var sentences = new List<String>();
+            int position = 0;
+            int start = 0;
+            // Extract sentences from the string.
+            do
             {
-                convbytes = Convert.FromBase64String(sIdtoken);
-            }
-            catch (Exception)
+                position = sIdtokenEnc.IndexOf('.', start);
+                if (position >= 0)
+                {
+                    sentences.Add(sIdtokenEnc.Substring(start, position - start).Trim());
+                    start = position + 1;
+                }
+            } while (position > 0);
+
+            // Display the sentences.
+            string sIdtokenJson = null;
+            foreach (string sentence in sentences)
             {
-
+                string sToDecode = sentence;
+                // do some padding fro the decoder
+                while (sToDecode.Length % 4 != 0)
+                {
+                    sToDecode += "=";
+                }
+                byte[] bDecoded = Convert.FromBase64String(sToDecode);
+                StringBuilder tmpStringBuilder = new StringBuilder();
+                tmpStringBuilder.Append(System.Text.UTF8Encoding.UTF8.GetChars(bDecoded));
+                string sDecoded = tmpStringBuilder.ToString();
+                if (sDecoded.Contains("email"))
+                {
+                    sIdtokenJson = sDecoded;
+                }
             }
-            //byte[] convbytes = Convert.FromBase64CharArray(sIdtoken.ToCharArray());
 
+            string username = null;
+            string firstname = null;
+            string lastname = null;
+            string email = null;
+            if (sIdtokenJson != null)
+            {
+                sIdtokenJson = sIdtokenJson.Replace("[", "");
+                sIdtokenJson = sIdtokenJson.Replace("]", "");
+                Dictionary<string, string> idtokenVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(sIdtokenJson);
+                foreach (var item in idtokenVals)
+                {
+                    string ikey = (string)item.Key;
+                    string ival = (string)item.Value;
+                    if (ikey.Contains("username"))
+                    {
+                        username = ival;
+                    }
+                    if (ikey.Contains("firstname"))
+                    {
+                        firstname = ival;
+                    }
+                    if (ikey.Contains("lastname"))
+                    {
+                        lastname = ival;
+                    }
+                    if (ikey.Contains("email"))
+                    {
+                        email = ival;
+                    }
+                }
+            }
 
-            /*
-             * 			var req = https.request(options, (res) => {
-                            res.setEncoding('utf-8');
-                            res.on('data', (chunk) => {
-                                //requestTC_JWT(JSON.parse(chunk).id_token);
-                                console.log("requestTID_JWT - response BODY:\n**********\n" + chunk + "\n**********\n");
-                                var id_token = JSON.parse(chunk).id_token;
-                                console.log("requestTID_JWT - response id_token:\n**********\n" + id_token + "\n**********\n");
-                                var id_token_buf = new Buffer(id_token, 'base64');
-                                console.log("requestTID_JWT - response id_token_buf tostring:\n**********\n" + id_token_buf.toString('utf8') + "\n**********\n");
-                                browserWindow.loadURL(redirectURL);
-                            });
-                            res.on('end', () => {
-                                console.log("requestTID_JWT - response: No more data in response.");
-                            });
-                        });
+            if (username == null)
+            {
+                return OAuthTokenResponse.Failed(new Exception("No username found in ID token."));
+            }
+            if (email == null)
+            {
+                return OAuthTokenResponse.Failed(new Exception("No email address found in ID token."));
+            }
 
-             */
-
+            payload["username"] = username;
+            payload["email"] = email;
+            if (firstname == null)
+            {
+                firstname = "<unknown firstname>";
+            }
+            payload["firstname"] = firstname;
+            if (lastname == null)
+            {
+                lastname = "<unknown lastname>";
+            }
+            payload["lastname"] = lastname;
 
             return OAuthTokenResponse.Success(payload);
         }
