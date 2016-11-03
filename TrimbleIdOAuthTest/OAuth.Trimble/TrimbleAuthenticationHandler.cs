90b20e99f4d8381571aeedfcb2481a97fdcc4f44
Andreas Lang <Andreas_Lang@trimble.com>
2016-11-03 16:16:03 +0100

    Cleanup

--- TrimbleIdOAuthTest/OAuth.Trimble/TrimbleAuthenticationHandler.cs
+++ TrimbleIdOAuthTest/OAuth.Trimble/TrimbleAuthenticationHandler.cs
@@ -26,11 +26,6 @@ namespace AspNet.Security.OAuth.Trimble {
             AuthenticationProperties properties, OAuthTokenResponse tokens) {
 
             JObject tokenResponseVals = tokens.Response;
-            //Dictionary<string, string> tokenResponseVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(tokenResponse);
-
-            //string sIdtokenEnc = tokenResponseVals.TryGetValue("id_token");
-            //tokenResponseVals.TryGetValue("id_token", out sIdtokenEnc);
-            //string sIdtokenEnc;
             JToken token = tokenResponseVals.GetValue("id_token");
             string sIdtokenEnc = token.ToString();
 
@@ -53,7 +48,7 @@ namespace AspNet.Security.OAuth.Trimble {
                 }
             } while (position > 0);
 
-            // Display the sentences.
+            // Decode the Base64 string and select the interesting part
             string sIdtokenJson = null;
             foreach (string sentence in sentences)
             {
@@ -73,6 +68,7 @@ namespace AspNet.Security.OAuth.Trimble {
                 }
             }
 
+            // pick the needed information from the JSON structure
             string username = null;
             string firstname = null;
             string lastname = null;
@@ -107,60 +103,47 @@ namespace AspNet.Security.OAuth.Trimble {
 
             // TODO: check mandatory stuff
 
-            if (username == null)
+            if ((username == null) || (email == null))
             {
-                //return OAuthTokenResponse.Failed(new Exception("No username found in ID token."));
-            }
-            if (email == null)
-            {
-                //return OAuthTokenResponse.Failed(new Exception("No email address found in ID token."));
+                return null;
             }
 
-            payload["username"] = username;
-            payload["email"] = email;
-            if (firstname == null)
-            {
-                firstname = "<unknown firstname>";
-            }
-            payload["firstname"] = firstname;
+            //payload["username"] = username;
+            //payload["email"] = email;
+            string fullname = null;
             if (lastname == null)
             {
                 lastname = "<unknown lastname>";
             }
-            payload["lastname"] = lastname;
+            else
+            {
+                fullname = lastname;
+            }
+            //payload["lastname"] = lastname;
+            if (firstname == null)
+            {
+                firstname = "<unknown firstname>";
+                if (fullname != null)
+                {
+                    fullname = firstname + " " + fullname;
+                }
+            }
+            //payload["firstname"] = firstname;
 
             // ToDo: add full name or something like this
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, TrimbleAuthenticationHelper.GetIdentifier(payload)));
-            identity.AddClaim(new Claim(ClaimTypes.Name, TrimbleAuthenticationHelper.GetName(payload)));
-            identity.AddClaim(new Claim(ClaimTypes.Email, TrimbleAuthenticationHelper.GetEmail(payload)));
-            //var principal = new ClaimsPrincipal(identity);
-
-
-
-            /**/
-
-            // Note: access tokens and request keys are passed in the querystring for Trimble
-            //var address = QueryHelpers.AddQueryString(Options.UserInformationEndpoint, new Dictionary<string, string>() {
-            //    ["access_token"] = tokens.AccessToken,
-            //    ["key"] = Options.RequestKey,
-            //    ["site"] = Options.Site
-            //});
-
-            //var request = new HttpRequestMessage(HttpMethod.Get, address);
-            //request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
-            //response.EnsureSuccessStatusCode();
-
-            //var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-            // Note: the email claim cannot be retrieved from Trimble's user information endpoint.
-            // Todo: implement
-            //identity.AddOptionalClaim(ClaimTypes.NameIdentifier, TrimbleAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer)
-            //        .AddOptionalClaim(ClaimTypes.Name, TrimbleAuthenticationHelper.GetDisplayName(payload), Options.ClaimsIssuer)
-            //        .AddOptionalClaim(ClaimTypes.Webpage, TrimbleAuthenticationHelper.GetWebsiteUrl(payload), Options.ClaimsIssuer)
-            //        .AddOptionalClaim("urn:Trimble:link", TrimbleAuthenticationHelper.GetLink(payload), Options.ClaimsIssuer);
-            //identity.AddClaims(ClaimTypes.NameIdentifier, TrimbleAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer);
+            //identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, TrimbleAuthenticationHelper.GetIdentifier(payload)));
+            //identity.AddClaim(new Claim(ClaimTypes.Name, TrimbleAuthenticationHelper.GetName(payload)));
+            //identity.AddClaim(new Claim(ClaimTypes.Email, TrimbleAuthenticationHelper.GetEmail(payload)));
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, username));
+            identity.AddClaim(new Claim(ClaimTypes.Email, email));
+            if (fullname == null)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Name, email));
+            }
+            else
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Name, fullname));
+            }
 
             var principal = new ClaimsPrincipal(identity);
             var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
@@ -169,7 +152,6 @@ namespace AspNet.Security.OAuth.Trimble {
             await Options.Events.CreatingTicket(context);
 
             return context.Ticket;
-            //return null;
         }
 
         protected override async Task<OAuthTokenResponse> ExchangeCodeAsync(string code, string redirectUri) {
@@ -198,121 +180,19 @@ namespace AspNet.Security.OAuth.Trimble {
 
                 return OAuthTokenResponse.Failed(new Exception("An error occurred when retrieving an access token."));
             }
-            //response.
 
             // Note: Trimble's token endpoint doesn't return JSON but uses application/x-www-form-urlencoded.
             // Since OAuthTokenResponse expects a JSON payload, a JObject is manually created using the returned values.
 
             string responsestr = await response.Content.ReadAsStringAsync();
             Dictionary<string, string> responseVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(responsestr);
-
-            //JsonConvert
             JObject payload = new JObject();
-            //var content = QueryHelpers.ParseQuery(responsestr);
-            //foreach (var item in content)
             foreach (var item in responseVals) {
-                string ikey = (string)item.Key;
-                string ival = (string)item.Value;
-                //payload[item.Key] = (string) item.Value;
-                payload[ikey] = ival;
+                payload[item.Key] = item.Value;
             }
 
-            //JToken id_token = payload.GetValue("id_token");
             string sIdtokenEnc;
             responseVals.TryGetValue("id_token", out sIdtokenEnc);
-            /*
-
-            // To use directly Convert.FromBase64String leads to invalid character exception.
-            // This is because there are illegal characters in it (".").
-            // That's why we use this as separator for making substrings. One of it includes the information we want.
-            var sentences = new List<String>();
-            int position = 0;
-            int start = 0;
-            // Extract sentences from the string.
-            do
-            {
-                position = sIdtokenEnc.IndexOf('.', start);
-                if (position >= 0)
-                {
-                    sentences.Add(sIdtokenEnc.Substring(start, position - start).Trim());
-                    start = position + 1;
-                }
-            } while (position > 0);
-
-            // Display the sentences.
-            string sIdtokenJson = null;
-            foreach (string sentence in sentences)
-            {
-                string sToDecode = sentence;
-                // do some padding fro the decoder
-                while (sToDecode.Length % 4 != 0)
-                {
-                    sToDecode += "=";
-                }
-                byte[] bDecoded = Convert.FromBase64String(sToDecode);
-                StringBuilder tmpStringBuilder = new StringBuilder();
-                tmpStringBuilder.Append(System.Text.UTF8Encoding.UTF8.GetChars(bDecoded));
-                string sDecoded = tmpStringBuilder.ToString();
-                if (sDecoded.Contains("email"))
-                {
-                    sIdtokenJson = sDecoded;
-                }
-            }
-
-            string username = null;
-            string firstname = null;
-            string lastname = null;
-            string email = null;
-            if (sIdtokenJson != null)
-            {
-                sIdtokenJson = sIdtokenJson.Replace("[", "");
-                sIdtokenJson = sIdtokenJson.Replace("]", "");
-                Dictionary<string, string> idtokenVals = JsonConvert.DeserializeObject<Dictionary<string, string>>(sIdtokenJson);
-                foreach (var item in idtokenVals)
-                {
-                    string ikey = (string)item.Key;
-                    string ival = (string)item.Value;
-                    if (ikey.Contains("username"))
-                    {
-                        username = ival;
-                    }
-                    if (ikey.Contains("firstname"))
-                    {
-                        firstname = ival;
-                    }
-                    if (ikey.Contains("lastname"))
-                    {
-                        lastname = ival;
-                    }
-                    if (ikey.Contains("email"))
-                    {
-                        email = ival;
-                    }
-                }
-            }
-
-            if (username == null)
-            {
-                return OAuthTokenResponse.Failed(new Exception("No username found in ID token."));
-            }
-            if (email == null)
-            {
-                return OAuthTokenResponse.Failed(new Exception("No email address found in ID token."));
-            }
-
-            payload["username"] = username;
-            payload["email"] = email;
-            if (firstname == null)
-            {
-                firstname = "<unknown firstname>";
-            }
-            payload["firstname"] = firstname;
-            if (lastname == null)
-            {
-                lastname = "<unknown lastname>";
-            }
-            payload["lastname"] = lastname;
-            */
 
             return OAuthTokenResponse.Success(payload);
         }
